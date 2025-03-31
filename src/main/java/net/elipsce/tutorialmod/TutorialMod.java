6a075fb822558051ca1864911cf5751efb07b9f3
Elipsce <simocurt9661@gmail.com>
2025-03-30 21:09:36 -0600

    Basic Tutorials Done

--- src/main/java/net/elipsce/tutorialmod/TutorialMod.java
+++ src/main/java/net/elipsce/tutorialmod/TutorialMod.java
@@ -1,5 +1,8 @@
 package net.elipsce.tutorialmod;
 
+import net.elipsce.tutorialmod.block.ModBlocks;
+import net.elipsce.tutorialmod.item.ModItemGroups;
+import net.elipsce.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +14,8 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+		ModItemGroups.registerItemGroups();
 	}
 }
